c8c72b71f295ea29f9d9f2aec91b47a3d03381e7
Alfons Caroles <cllin7787@gmail.com>
2017-03-05 00:13:16 +1100

    finished layout

--- main.js
+++ main.js
@@ -13,15 +13,28 @@ function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slideImg");
   var dots = document.getElementsByClassName("indicator");
-  if (n > x.length) {slideIndex = 1}    
+  if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" selected", "");
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " selected";
 }
 
+$( '.button' ).click(function(event) {
+  var button = event.target;
+  var slideArrow = $(button).next();
+  var article = $('article.' + $(button).attr('class'));
+
+  if ($(slideArrow).html() === $(slideArrow).html("&#10836;").text()) {
+    $(slideArrow).html("&#10835;");
+    article.css('display', 'block');
+  } else {
+    $(slideArrow).html("&#10836;");
+    article.css('display', 'none');
+  }
+});
